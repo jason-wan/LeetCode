a4a6024eb45993ec7ed6f9dc470de15f9c9593ee
Jason Wan <junjun.wanjun@gmail.com>
2014-06-24 08:01:11 -0700

    Remove Duplicates from Sorted List

---
+++ remove_duplicate_sort_list.c
@@ -0,0 +1,23 @@
+class Solution {
+
+public:
+	ListNode *deleteDuplicates(ListNode *head){
+		ListNode* cur = head;
+		if (!cur)
+			return head;
+		ListNode* next = cur->next;
+
+		while (next){
+			if (cur->val == next->val) {
+				cur->next = next->next;
+				delete next;
+			}
+			else {
+				cur = next;
+			}
+			next = next->next;
+		}
+
+		return head;
+	}
+}
